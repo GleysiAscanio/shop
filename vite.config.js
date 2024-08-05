b1583d3e6a59652f63cc2c54cc25b3d590433cad
Gleysi <gleysiascanio29@gmail.com>
2024-08-05 13:54:09 -0600

    recuperando visibilidad

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { vikePlugin } from 'vike/plugin'
 import vike from 'vike/plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vike({ prerender: true })
-    // vikePlugin({ prerender: true })
+  plugins: [react(),
+     vike({ prerender: true })
   ],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
