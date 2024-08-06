fa3f156ec81e71fd23b01f936be61a2d2c12c7f6
Gleysi <gleysiascanio29@gmail.com>
2024-08-06 14:19:07 -0600

    recuperando archivo de config vercel2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 
 export default defineConfig({
-  plugins: [react(), vike()],
+  plugins: [react(), vike({ prerender: true })],
   build: {
     outDir: 'dist'
   }
